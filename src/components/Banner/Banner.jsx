f79d0031dd4984d40f6abf36bab4da87e3cc6099
Hussain Ali <iamhussainali2@gmail.com>
2024-02-23 23:24:32 +0530

    changes some mobile friendly design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -61,7 +61,7 @@ const Banner = () => {
       )}
     </nav>
     <div>
-      <h1 className=" text-center text-3xl font-bold mt-52">I Grow By Helping People In Need</h1>
+      <h1 className=" text-center text-xl md:text-3xl font-bold mt-32 md:mt-52">I Grow By Helping People In Need</h1>
       <div className=" flex justify-center mt-12">
         <input placeholder="Search here...." className=" px-2 relative left-2 border border-r-0 rounded-l-md  border-gray-400 py-1" type="text" /> <button className="  bg-red-500 px-6 py-1 text-white rounded-r-md">Search</button>
       </div>
